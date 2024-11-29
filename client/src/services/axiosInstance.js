7d9b9d7edbd58d5e563b79e0e7549fefc5a460ff
Adii Malik <adeed.cs@gmail.com>
2024-11-30 01:44:38 +0500

    Building the app with .env files

--- client/src/services/axiosInstance.js
+++ client/src/services/axiosInstance.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-console.log(process.env.REACT_APP_API_URL);
+console.log(process.env);
 
 // Create an Axios instance
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: process.env.REACT_APP_API_URL || 'https://rosca-app-8wmr3q.fly.dev/api',
     headers: {
         'Content-Type': 'application/json',
     },
