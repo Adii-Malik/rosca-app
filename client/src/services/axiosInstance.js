a867a865f3c7b4370ef0f11ffca81b411f162ad0
Adii Malik <adeed.cs@gmail.com>
2024-11-30 01:22:57 +0500

    Building the app with .env files

--- client/src/services/axiosInstance.js
+++ client/src/services/axiosInstance.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+console.log(process.env.REACT_APP_API_URL);
+
 // Create an Axios instance
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
